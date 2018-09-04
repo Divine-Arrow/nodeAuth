3d7a48e7c6a15bb78218ce55440d306bceeec983
Bhupender Singh <bhupenders225@gmail.com>
2018-09-05 01:03:04 +0530

    adding logout bugfix3

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-var db = process.env.MONGODB_URI || 'mongodb://localhost/userData';
-mongoose.connect('mongodb://localhost/userData', { useNewUrlParser: true});
+var uriString = process.env.MONGODB_URI || 'mongodb://localhost/userData';
+
+mongoose.connect(uriString);
 
 module.exports = {mongoose}
